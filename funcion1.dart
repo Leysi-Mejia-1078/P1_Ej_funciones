ffcb27eef5cb39e6f662ecf571f62fef1f5c319c
Leysi Mejia <164534001+Leysi-Mejia-1078@users.noreply.github.com>
2025-02-07 16:47:42 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,21 @@
+// Función que no devuelve valor
+// Función saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribirNombre(){
+  print('Leysi Mejia');
+}
+
+void main(){
+  // Leysi Mejia  Mat: 22308051281078
+  // Llamar a la función saludar
+  print('Leysi Mejia  Mat: 22308051281078');
+  print('Llamando a la función saludar');
+  saludar();
+  print('Llamando a la función escribirNombre');
+  escribirNombre();
+
+  
+}
