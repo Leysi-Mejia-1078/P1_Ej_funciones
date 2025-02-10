77178f60f9718ce5186ce412b664cd8463b460fa
Leysi Mejia <164534001+Leysi-Mejia-1078@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,17 @@
+// Funciones creadas por el usuario
+void suma(int a, int b){
+  print('La suma es: ${a + b}');
+}
+
+void resta(int a, int b){
+  print('La resta es: ${a - b}');
+}
+
+// Función main
+void main(){
+  // Leysi Mejia  Mat: 22308051281078
+  print('Leysi Mejia  Mat: 22308051281078');
+  print('Llamando a la función suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
